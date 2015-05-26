3f2b6a9e997be562cf8d3fd6252fe9415f6476e5
Pedro Assis <pedro@pedroassis.com.br>
2015-05-26 20:52:38 -0300

    Added FaixaConsumo

---
+++ TI/Entidade/FaixaConsumo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TI.Entidade
+{
+    class FaixaConsumo
+    {
+        public Int32    Minimo          { get; set; }
+        public Int32    Maximo          { get; set; }
+        public Double   Agua            { get; set; }
+        public Double   Esgoto          { get; set; }
+        public Int32    AguaFixo        { get; set; }
+        public Int32    EsgotoFixo      { get; set; }
+    }
+}
