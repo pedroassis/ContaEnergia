3a49d655fc1bf3d61f7004e573aebdb00b9ca1fd
Felipe Leal <fhleal@outlook.com>
2015-05-28 00:44:28 -0300

    UI update

--- TI/View/MenuChooser.Designer.cs
+++ TI/View/MenuChooser.Designer.cs
@@ -224,6 +224,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MenuChooser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Menu_FormClosing);
             this.Load += new System.EventHandler(this.Menu_Load);
