7e91f5eb80b1a7b8391d6df7be4f5e5d2619a39a
Felipe Leal <fhleal@outlook.com>
2015-05-28 00:39:39 -0300

    Added configuration to read CVS imported files

--- TI/Service/CSVImport.cs
+++ TI/Service/CSVImport.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Linq;
 using System.IO;
 using System.Collections.Generic;
@@ -11,19 +12,29 @@ namespace TI
 		{
 		}
 
-		public List<T> Import<T>(String file, String[] colunms){
+		public List<T> Import<T>(String file, List<Func<string, T, object>> parsers, String[] colunms){
 			Type type = typeof(T);
 
 			string[] lines = File.ReadAllLines(file);
 
-			return lines.ToList ().Select (line => Parse<T>(line, type, colunms)).ToList();
+			return lines.ToList ().Select (line => Parse<T>(line, type, colunms, parsers)).ToList();
 		}
 
-		private T Parse<T>(String line, Type type, String[] colunms){
+        private T Parse<T>(String line, Type type, String[] colunms, List<Func<string, T, object>> parsers)
+        {
 			T dataSourceObject = (T)Activator.CreateInstance(type);
 			String[] items = line.Split (';');
 			for (int i = 0; i < items.Length; i++) {
-				colunms.ToList ().ForEach (prop => type.GetProperty(colunms[i]).SetValue (dataSourceObject, items[i]));
+                object toSet;
+                try {
+                    toSet = parsers.ElementAt(i)(items[i], dataSourceObject);
+                }
+                catch (Exception e)
+                {
+                    PropertyInfo propType = type.GetProperty(colunms[i]);
+                    toSet = typeof(String).IsAssignableFrom(propType.PropertyType) ? items[i] : propType.PropertyType.GetMethod("Parse", new[] { typeof(string) }).Invoke(null, new String[] { (String)items[i] });
+                }
+				type.GetProperty(colunms[i]).SetValue (dataSourceObject, toSet);
 			}
 			return dataSourceObject;
 		}
