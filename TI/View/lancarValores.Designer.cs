ecca467d0cf24aa2e16b2c9ca9ea8ee4f58871a0
Felipe Leal <fhleal@outlook.com>
2015-05-28 00:55:58 -0300

    added upload button

--- TI/View/lancarValores.Designer.cs
+++ TI/View/lancarValores.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TI.View
 {
-    partial class lancarValores
+    partial class LancarValores
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lancarValores));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LancarValores));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.txtTipo = new System.Windows.Forms.TextBox();
@@ -50,13 +50,19 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.txtTipo);
             this.panel1.Controls.Add(this.groupBox1);
@@ -72,26 +78,28 @@
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(768, 298);
+            this.panel1.Size = new System.Drawing.Size(768, 334);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(583, 13);
+            this.label1.Location = new System.Drawing.Point(469, 65);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 20);
             this.label1.TabIndex = 54;
             this.label1.Text = "Tipo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtTipo
             // 
             this.txtTipo.BackColor = System.Drawing.Color.Gainsboro;
             this.txtTipo.Enabled = false;
-            this.txtTipo.Location = new System.Drawing.Point(587, 36);
+            this.txtTipo.Location = new System.Drawing.Point(473, 88);
             this.txtTipo.Name = "txtTipo";
             this.txtTipo.Size = new System.Drawing.Size(165, 26);
             this.txtTipo.TabIndex = 55;
+            this.txtTipo.TextChanged += new System.EventHandler(this.txtTipo_TextChanged);
             // 
             // groupBox1
             // 
@@ -104,9 +112,9 @@
             this.groupBox1.Controls.Add(this.valorContaAnterior);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.consumoAnterior);
-            this.groupBox1.Location = new System.Drawing.Point(22, 80);
+            this.groupBox1.Location = new System.Drawing.Point(22, 124);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(730, 167);
+            this.groupBox1.Size = new System.Drawing.Size(730, 164);
             this.groupBox1.TabIndex = 53;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Dados de consumo";
@@ -115,7 +123,7 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(451, 129);
+            this.button5.Location = new System.Drawing.Point(476, 127);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(124, 26);
             this.button5.TabIndex = 54;
@@ -126,7 +134,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(231, 106);
+            this.label4.Location = new System.Drawing.Point(231, 104);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(147, 20);
             this.label4.TabIndex = 58;
@@ -136,7 +144,7 @@
             // 
             this.valorContaAtual.BackColor = System.Drawing.Color.Gainsboro;
             this.valorContaAtual.Enabled = false;
-            this.valorContaAtual.Location = new System.Drawing.Point(235, 129);
+            this.valorContaAtual.Location = new System.Drawing.Point(235, 127);
             this.valorContaAtual.Name = "valorContaAtual";
             this.valorContaAtual.Size = new System.Drawing.Size(210, 26);
             this.valorContaAtual.TabIndex = 59;
@@ -144,7 +152,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(11, 106);
+            this.label3.Location = new System.Drawing.Point(11, 104);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(97, 20);
             this.label3.TabIndex = 56;
@@ -152,7 +160,7 @@
             // 
             // consumoAtual
             // 
-            this.consumoAtual.Location = new System.Drawing.Point(15, 129);
+            this.consumoAtual.Location = new System.Drawing.Point(15, 127);
             this.consumoAtual.Name = "consumoAtual";
             this.consumoAtual.Size = new System.Drawing.Size(210, 26);
             this.consumoAtual.TabIndex = 57;
@@ -160,7 +168,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(227, 37);
+            this.label6.Location = new System.Drawing.Point(227, 48);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(170, 20);
             this.label6.TabIndex = 54;
@@ -169,7 +177,7 @@
             // valorContaAnterior
             // 
             this.valorContaAnterior.BackColor = System.Drawing.Color.LightGray;
-            this.valorContaAnterior.Location = new System.Drawing.Point(231, 60);
+            this.valorContaAnterior.Location = new System.Drawing.Point(231, 71);
             this.valorContaAnterior.Name = "valorContaAnterior";
             this.valorContaAnterior.Size = new System.Drawing.Size(210, 26);
             this.valorContaAnterior.TabIndex = 55;
@@ -177,7 +185,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(11, 37);
+            this.label5.Location = new System.Drawing.Point(11, 48);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(116, 20);
             this.label5.TabIndex = 54;
@@ -186,7 +194,7 @@
             // consumoAnterior
             // 
             this.consumoAnterior.BackColor = System.Drawing.Color.LightGray;
-            this.consumoAnterior.Location = new System.Drawing.Point(15, 60);
+            this.consumoAnterior.Location = new System.Drawing.Point(15, 71);
             this.consumoAnterior.Name = "consumoAnterior";
             this.consumoAnterior.Size = new System.Drawing.Size(210, 26);
             this.consumoAnterior.TabIndex = 55;
@@ -221,7 +229,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 13);
+            this.label2.Location = new System.Drawing.Point(20, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(51, 20);
             this.label2.TabIndex = 43;
@@ -232,7 +240,7 @@
             // 
             this.txtNome.BackColor = System.Drawing.Color.Gainsboro;
             this.txtNome.Enabled = false;
-            this.txtNome.Location = new System.Drawing.Point(129, 36);
+            this.txtNome.Location = new System.Drawing.Point(24, 88);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(443, 26);
             this.txtNome.TabIndex = 44;
@@ -241,18 +249,18 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(22, 262);
+            this.button4.Location = new System.Drawing.Point(22, 296);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 26);
             this.button4.TabIndex = 32;
-            this.button4.Text = "Sair";
+            this.button4.Text = "Voltar";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(498, 262);
+            this.button2.Location = new System.Drawing.Point(498, 294);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(124, 26);
             this.button2.TabIndex = 30;
@@ -263,7 +271,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(628, 262);
+            this.button1.Location = new System.Drawing.Point(632, 294);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 26);
             this.button1.TabIndex = 28;
@@ -271,17 +279,40 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // lancarValores
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(662, 25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(42, 38);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 57;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(710, 25);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(42, 37);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 56;
+            this.pictureBox3.TabStop = false;
+            // 
+            // LancarValores
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(768, 298);
+            this.ClientSize = new System.Drawing.Size(768, 334);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "lancarValores";
+            this.Name = "LancarValores";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lançar Valores de Conta";
             this.Load += new System.EventHandler(this.lancarValores_Load);
             this.panel1.ResumeLayout(false);
@@ -289,6 +320,8 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -316,6 +349,8 @@
         private System.Windows.Forms.TextBox valorContaAtual;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
 
 
     }
