91efc24183602fc0a6f9ae032d33c5350393990e
Felipe Leal <fhleal@outlook.com>
2015-05-31 18:26:57 -0300

    anything

--- TI/Program.cs
+++ TI/Program.cs
@@ -9,30 +9,31 @@ using System.Windows.Forms;
 using System.Diagnostics;
 namespace TI
 {
-	class MainClass
-	{
+    class MainClass
+    {
         [STAThreadAttribute]
-		public static void Main(){
-			ContaCSVImporter csv = new ContaCSVImporter ();
-			Stopwatch stopwatch = new Stopwatch();
+        public static void Main()
+        {
+            //ContaCSVImporter csv = new ContaCSVImporter();
+            //Stopwatch stopwatch = new Stopwatch();
 
-			// Begin timing
-			stopwatch.Start();
+            //// Begin timing
+            //stopwatch.Start();
 
-			List<Conta> c = csv.Import ("/Users/ac-passis/Downloads/contasV2.txt", new string[] { "TipoConta", "Consumidor", "Data", "LeituraAnterior", "LeituraAtual" });
+            //List<Conta> c = csv.Import ("/Users/ac-passis/Downloads/contasV2.txt", new string[] { "TipoConta", "Consumidor", "Data", "LeituraAnterior", "LeituraAtual" });
 
-			// Stop timing
-			stopwatch.Stop();
+            //// Stop timing
+            //stopwatch.Stop();
 
-			// Write result
-			Console.WriteLine("Time elapsed: {0}",
-				stopwatch.Elapsed);
-			Console.WriteLine ("Numero " + c.Count);
-			Console.ReadLine ();
-//			Application.EnableVisualStyles();
-//            Application.SetCompatibleTextRenderingDefault(false);
-//            Application.Run(new TI.View.splash());
-			           
+            //// Write result
+            //Console.WriteLine("Time elapsed: {0}",
+            //    stopwatch.Elapsed);
+            //Console.WriteLine ("Numero " + c.Count);
+            //Console.ReadLine ();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TI.View.splash());
+
         }
-	}
+    }
 }
